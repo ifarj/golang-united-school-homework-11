43f2da9ad43c38f7d2e8b95b424ff909ba9e1698
Berik Akhmed <a.berik@bsl.dev>
2022-07-18 16:21:55 +0600

    add task11

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,24 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	ch := make(chan int64, pool)
+	res = []user{}
+	for i := int64(0); i < n; i++ {
+		wg.Add(1)
+		ch <- i
+		go func() {
+			user := getOne(<-ch)
+			mu.Lock()
+			res = append(res, user)
+			mu.Unlock()
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	time.Sleep(time.Duration(n*100/pool*time.Millisecond.Nanoseconds() - time.Millisecond.Nanoseconds()*100))
+
+	return res
 }
